ee54b7aba676fbd12e52fe0ceaffd30dca86d710
gnicholas012 <43080790+gnicholas012@users.noreply.github.com>
2018-09-10 23:59:41 -0400

    Added Score
    Added a variable and a function that keeps track of how many pick ups have been collected and displays the amount on a text element. When all pick ups are collected, text displays "mission complete" and turns yellow.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,15 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
     public const float Speed = 10;
     private Rigidbody rb;
+    private const int Total = 8;
+    private int Count = 0;
+    public Text CountText;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+        setCountText();
     }
 
     void FixedUpdate()
@@ -26,6 +31,21 @@ public class PlayerController : MonoBehaviour
         if(other.gameObject.CompareTag("PickUp"))
         {
             other.gameObject.SetActive(false);
+            Count++;
+        }
+        setCountText();
+    }
+
+    void setCountText()
+    {
+        if (Count >= Total)
+        {
+            CountText.color = Color.yellow;
+            CountText.text = "Count: " + Count.ToString() + "/" + Total.ToString() + " Mission complete!";
+        }
+        else
+        {
+            CountText.text = "Count: " + Count.ToString() + "/" + Total.ToString();
         }
     }
 }
