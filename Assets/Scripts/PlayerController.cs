4a648933967b7e883750205a85c0af25e7e97085
gnicholas012 <43080790+gnicholas012@users.noreply.github.com>
2018-09-10 22:03:05 -0400

    Added Player Movent
    Added a RigidBody to the sphere and added code to apply forces to the sphere to move it with a constant speed multiplier.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public const float Speed = 10;
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float MoveHorizontal = Input.GetAxis("Horizontal");
+        float MoveVertical = Input.GetAxis("Vertical");
+
+        Vector3 Movement = new Vector3(MoveHorizontal, 0.0f, MoveVertical);
+        rb.AddForce(Movement * Speed);
+    }
+}
